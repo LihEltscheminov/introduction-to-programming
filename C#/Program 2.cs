b5eea8aa5687699fc84beda655314adfb1e5dc78
Alice Eltscheminov <77692726+LihEltscheminov@users.noreply.github.com>
2021-09-06 14:25:13 +0200

    Add files via upload

---
+++ C#/Program 2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P1_Vecka2_GenomGang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string namn, input;
+            int alder;
+            float langd, vikt;
+            
+
+            Console.WriteLine("Hej! Vad heter du?");
+            namn = Console.ReadLine();
+            Console.WriteLine("Hejsan " + namn + "!");
+
+            Console.WriteLine("Hur gammal är du?");
+            input = Console.ReadLine();
+            Int32.TryParse(input, out alder);
+            Console.WriteLine("Ok, din ålder är " + alder + ".");
+
+            Console.WriteLine("Hur lång är du i cm?");
+            langd = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ok, din langd är " + langd + "cm.");
+            float langdMeter = langd / 100;
+
+            Console.WriteLine("Hur mycket väger du i kg?");
+            vikt = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ok, du väger " + vikt + "kg.");
+
+            float bmi = vikt / (langdMeter * langdMeter);
+            Console.WriteLine(namn + ", du är " + alder + "år, " + langdMeter + "m lång och väger " + vikt + "kg. Din beräknade BMI är " + Math.Round(bmi,1)+ ".");
+
+            }
+    }
+}
---
+++ C#/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+    namespace UPG3_Program1
+    {
+        class Program
+        {
+            static bool programQuit = false;
+
+        static void Main(string[] args)
+            {
+                while (programQuit == false)
+                {
+                MainMenuText();
+                }
+            static void MainMenuText()
+            {
+                Console.WriteLine("Hej, Gör ditt val:");
+                Console.WriteLine("1: Skriv ut 1 till 100.");
+                Console.WriteLine("2: Skriv ut 100 till 1.");
+                Console.WriteLine("3: Avsluta program");
+                string userChoice = Console.ReadLine();
+            
+                if (userChoice == "1")
+                {
+                    Console.WriteLine("Du har gjort val 1");
+                    Counter(true);
+                    KeepTrying();
+                }
+                else if (userChoice == "2")
+                {
+                    Console.WriteLine("Du har gjort val 2");
+                    Counter(false);
+                    KeepTrying();
+                }
+                else if (userChoice == "3")
+                {
+                    Console.WriteLine("Programmet avslutas");
+                    programQuit = true;
+                }
+                else
+                {
+                    if (true)
+                    {
+                    Console.WriteLine("Ditt val är ej giltigt, prova igen.");
+                    MainMenuText();
+                    }
+                }
+            }
+
+            static void KeepTrying()
+            {
+                Console.WriteLine("vill du försöka igen ?");
+                Console.WriteLine("*: För ja.");
+                Console.WriteLine("#: För nej.");
+                string userChoiceAgain = Console.ReadLine();
+
+                if (userChoiceAgain == "*")
+                {
+                    Console.WriteLine("Du kommer att spela igen.");
+                    MainMenuText();
+                }
+                else if (userChoiceAgain == "#")
+                {
+                    Console.WriteLine("Programmet avslutas");
+                    programQuit = true;
+                }
+                else
+                {
+                    Console.WriteLine("Ditt val är ej giltigt, prova igen.");
+                    KeepTrying();
+                }
+
+            }
+
+
+            static void Counter (bool countUp)
+            {
+                    if(countUp==true)
+                    {
+                        for(int i=0; i<100; i++)
+                        {
+                            Console.WriteLine(i + 1);
+                        }
+                    }
+                    else
+                    {
+                        for(int i=100; i>0; i--)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+            }
+            }
+        }
+    }
+
+
+
